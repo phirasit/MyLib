8bef6b4236c068e308e08f0541fa5f424e11bd6d
Phirasit <phirasitsharp@gmail.com>
2015-05-03 18:44:06 +0700

    add lis functions into DP.cpp

--- DP.cpp
+++ DP.cpp
@@ -8,16 +8,17 @@
 using namespace std;
 
 namespace DP {
-  unsigned lis(vector<int>);
+  template<typename> unsigned lis(vector<int>);
+  template<typename> unsigned lis_nlogn(vector<int>);
 };
 
-unsigned DP::lis(vector<int> arr) {
+template<typename Data> unsigned DP::lis(vector<Data> arr) {
   if(arr.size() == 0u) {
     return 0u;
   }
-  vector<unsigned> dp(arr, 0u);
+  vector<unsigned> dp(arr.size(), 0u);
   unsigned ans = 0u;
-  for(int i = arr.size() - 1;i >= 0;i++) {
+  for(int i = arr.size() - 1;i >= 0;i--) {
     dp[i] = 1u;
     for(int j = i+1;j < arr.size();j++) {
       if(arr[j] > arr[i]) {
@@ -28,5 +29,30 @@ unsigned DP::lis(vector<int> arr) {
   }
   return ans;
 }
-
+template<typename Data> unsigned DP::lis_nlogn(vector<Data> arr) {
+  vector<Data> dp;
+  for(Data val : arr) {
+    if(dp.size() == 0u) {
+      dp.push_back(val);
+    }else if(val <= dp[0]) {
+      dp[0] = val;
+    }else {
+      int a = 0, b = dp.size() - 1;
+      while(a < b) {
+        int mid = (a + b) / 2 + 1;
+        if(val >= dp[mid]) {
+          a = mid;
+        }else {
+          b = mid - 1;
+        }
+      }
+      if(a+1 == dp.size()) {
+        dp.push_back(val);
+      }else {
+        dp[a+1] = val;  
+      }
+    }
+  }
+  return dp.size();
+}
 #endif
