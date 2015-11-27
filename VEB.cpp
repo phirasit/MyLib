89c2ef554d6ac23258b717b507c368efcdd296fa
Phirasit Charoenchitseriwong <phirasitsharp@gmail.com>
2015-11-27 16:00:34 +0700

    First version of Van embe boas tree (unfinished)

---
+++ VEB.cpp
@@ -0,0 +1,28 @@
+/*
+Van embe boas tree
+Implemented by : Phirasit Charoenchitseriwong
+
+This tree performs better than normal BSTs
+with running time of O(log log U) instead of
+O(log N). This result leads to a new approach
+for dealing with dictionary.
+
+VEB consists of exploiting bit and also resursive tree.
+This tree is not an improved one so the memory require
+is O(U log log U) however this can improve easily.
+
+Everyone is free to use this code.
+The author personally believe that the code is bug-free.
+*/
+
+#ifndef VAN_EMBE_BOAS 
+
+#define VAN_EMBE_BOAS
+
+class VEB {
+	public :
+		class node {
+		};
+};
+
+#endif
